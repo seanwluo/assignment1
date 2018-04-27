ef138c6ca6c852facbfde370b1aa854d021ef0bd
Raj Kumar Shrestha <s3675667@student.rmit.edu.au>
2018-04-27 21:00:07 +1000

    DBUtil class to connect database

---
+++ src/db/DBUtil.java
@@ -0,0 +1,57 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.hsqldb.Server;
+
+/**
+ * 
+ * @author Raj
+ *
+ */
+public class DBUtil 
+{	
+	private final static String DB_NAME = "MiniNetDB";
+	private static Connection _dbConnection = null;
+	
+	public boolean connect() {
+		if(_dbConnection != null)
+		{
+			System.out.println("DB already connected");
+			return true;
+		} else {
+			// return true on successful connection
+			return stablishConnection();
+		}
+	}
+	
+	/**
+	 * 
+	 * @return true if successfully connect to db
+	 */
+	private boolean stablishConnection() {
+		try 
+		{
+			Class.forName("org.hsqldb.jdbcDriver");
+			_dbConnection =
+			DriverManager.getConnection("jdbc:hsqldb:" + DB_NAME, "user", "pass123");
+			_dbConnection.commit();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		} catch (ClassNotFoundException e2) {
+			e2.printStackTrace();
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public Connection getConnection()
+	{
+		return _dbConnection;
+	}
+
+}
