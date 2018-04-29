56564555fc2d4beaf2ce7c56ecaecd2390dbd240
Raj Kumar Shrestha <s3675667@student.rmit.edu.au>
2018-04-30 08:48:39 +1000

    create profile table

--- src/db/CreateTables.java
+++ src/db/CreateTables.java
@@ -2,12 +2,13 @@ package db;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedHashMap;
+
+import models.User;
 
 public class CreateTables {
 	private Connection _dbConnection = null;
-	private Map<String, String> tableQuries = new HashMap<String, String>();
+	private LinkedHashMap<String, String> tableQuries = new LinkedHashMap<String, String>();
 	private HSQLServer _server = null;
 	
 	public CreateTables() {
@@ -28,6 +29,7 @@ public class CreateTables {
 		{	
 			_dbConnection.prepareStatement("drop table " + tableName + " if exists;").execute();
 			
+			// Build table create query
 			createQuery = String.format("create table %s (%s);", tableName, tableQuries.get(tableName));
 			_dbConnection.prepareStatement(createQuery).execute();
 		}
@@ -36,7 +38,28 @@ public class CreateTables {
 	
 	private void buildTablesQuery()
 	{	
-		tableQuries.put("users", "username varchar(25) not null, type varchar(5), primary key (username)");
+		tableQuries.put("users", "username varchar(25) not null, password varchar(25), type varchar(5), primary key (username)");
+
+		tableQuries.put("profiles", 
+				"id integer identity, "
+				+ "user varchar(25) not null, "
+				+ "firstname varchar(20) not null, "
+				+ "lastname varchar(20) not null, "
+				+ "age integer, "
+				+ "gender varchar(6), "
+				+ "status varchar(30), "
+				+ "picUrl varchar(100), "
+				+ "primary key (id, user), "
+				+ "foreign key(user) references users(username)");
+		
+		tableQuries.put("friendships",
+				"id integer identity, "
+				+ "user1 varchar(25) not null, "
+				+ "user2 varchar(25) not null, "
+				+ "type varchar(10), "
+				+ "primary key(id, user1, user2), "
+				+ "foreign key(user1) references users(username), "
+				+ "foreign key(user2) references users(username)");
 	}
 	
 	public static void main(String[] args) throws SQLException
