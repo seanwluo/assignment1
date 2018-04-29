3a41b043d63c47c994e9f864381d60d45531827f
Raj Kumar Shrestha <s3675667@student.rmit.edu.au>
2018-04-30 07:44:15 +1000

    add server stop method

--- src/db/HSQLServer.java
+++ src/db/HSQLServer.java
@@ -8,19 +8,33 @@ import org.hsqldb.Server;
  * @author Raj
  *
  */
-public class HSQLServer {
+public class HSQLServer
+{
 	private final static String DB_NAME = "MiniNetDB";
 	private final static String DB_PATH = "file:MYDB";
 	private Server _hsqlServer = null;
 	
-	public void start()
+	public HSQLServer()
 	{
 		_hsqlServer = new Server();
 		_hsqlServer.setLogWriter(null);
 		_hsqlServer.setSilent(true);
 		_hsqlServer.setDatabaseName(0, DB_NAME);
 		_hsqlServer.setDatabasePath(0, DB_PATH);
+	}
+	
+	public void start()
+	{
 		_hsqlServer.start();
+	}
+	
+	public void stop()
+	{	
+		if (_hsqlServer != null) {
+            _hsqlServer.stop();
+        }else {
+        	System.out.println("Server not found");
+        }
 	}
 	
 	public Server getServer()
