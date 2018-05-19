205cbe76bb879a88dcfb702fcd5a5a626c3cff31
Raj Kumar Shrestha <s3675667@student.rmit.edu.au>
2018-05-19 21:11:45 +1000

    Add file service to read file

---
+++ src/Services/FileService.java
@@ -0,0 +1,46 @@
+package Services;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileService
+{
+	private String _fileName;
+	
+	public  FileService(String fileName)
+	{
+		this._fileName = fileName;
+	}
+	//check file exists
+	
+	/*
+	 * Create method to read from file
+	 */
+	public List<String> read()
+	{
+		List<String> dataArray = new ArrayList<String>();
+		
+		try {
+		
+			BufferedReader input = new BufferedReader(new FileReader (_fileName));
+		    String data = null;
+		    
+		    do {
+				data = input.readLine();
+				if (data != null)
+				{
+					dataArray.add(data);
+				}
+			} while (data != null);
+			
+			input.close();
+		} catch (IOException e) {
+			System.out.println("File Reading Error!!");
+		}
+		
+		return dataArray;
+	}
+}
